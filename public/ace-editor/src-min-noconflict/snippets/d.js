478ead602b08c10ebb2042fd663f3eb5a3fb36aa
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-02-08 22:30:58 +0100

    All js and css is now managed by webpack! Yay!

--- public/ace-editor/src-min-noconflict/snippets/d.js
+++
@@ -1,4 +0,0 @@
-ace.define("ace/snippets/d", ["require", "exports", "module"], function (e, t, n) {
-    "use strict";
-    t.snippetText = undefined, t.scope = "d"
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,46 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+
 module.exports = {
-    entry: "./app/javascripts/js/app.js",
-    output: {
-        path: __dirname + "/public/javascripts",
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['./babelRelayPlugin']
-                }
-            }
-        ]
+  entry: {
+    app: "./app/javascripts/app.jsx",
+    index: ['bootstrap-loader/extractStyles', "./app/javascripts/index.js"]
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    path: __dirname + "/public/",
+    publicPath: "/assets/",
+    filename: "[name].js"
+  },
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js'],
+    alias: {
+      history: 'historyjs/scripts/bundled-uncompressed/html4+html5/jquery.history'
     }
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css', {allChunks: true}),
+    new webpack.ProvidePlugin({"window.jQuery": "jquery"}) // history.js expects it :(
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['./babelRelayPlugin']
+        }
+      },
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css?sourceMap&modules!sass?sourceMap")},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css')},
+      {test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000'},
+      {test: /bootstrap-sass[\\\/].*\.js/, loader: 'imports?jQuery=jquery'},
+      {test: /jquery.hotkeys[\\\/].*\.js/, loader: 'imports?jQuery=jquery'}
+    ]
+  }
 };

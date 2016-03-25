9e4ecaeea11578a054f1b4a675ae99b84d716c4e
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-03-25 03:41:20 +0100

    Use operationName

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -18,7 +18,7 @@ $(function () {
       .fail(function (error) {
         console.error(error)
       })
-  }
+  };
 
   var examples = {
     factionsAndShips: {
@@ -37,74 +37,74 @@ $(function () {
     fragmentsExample: {
       query: 'query FragmentExample {\n  rebels {\n    ...Common\n  }\n\n  empire {\n    ...Common\n  }\n}\n\nfragment Common on Faction {\n  id\n  name\n}'
     }
-  }
+  };
 
-  var editorTheme = "ace/theme/github"
-  var initialQuery = "query FactionsAndShips {\n  factions(names:[\"empire\",\"rebels\"]) {\n    name\n    ships{\n      edges{\n        node{\n          name\n        }\n      }\n    }\n  }\n}"
+  var editorTheme = "ace/theme/github";
+  var initialQuery = "query FactionsAndShips {\n  factions(names:[\"empire\",\"rebels\"]) {\n    name\n    ships{\n      edges{\n        node{\n          name\n        }\n      }\n    }\n  }\n}";
 
   var createEditor = function (id, mode, readOnly) {
     var editor = ace.edit(id);
 
-    editor.setTheme(editorTheme)
+    editor.setTheme(editorTheme);
     editor.getSession().setOptions({
       mode: "ace/mode/" + mode,
       tabSize: 2,
       useSoftTabs: true
     });
-    editor.setDisplayIndentGuides(true)
-    editor.setFontSize(17)
+    editor.setDisplayIndentGuides(true);
+    editor.setFontSize(17);
 
-    if (readOnly) editor.setReadOnly(readOnly)
+    if (readOnly) editor.setReadOnly(readOnly);
 
     return editor
-  }
+  };
 
-  updateSchema()
+  updateSchema();
 
-  var queryEditor = createEditor("queryEditor", "text")
-  var variablesEditor = createEditor("variablesEditor", "json")
-  var responseEditor = createEditor("responseEditor", "json", true)
+  var queryEditor = createEditor("queryEditor", "text");
+  var variablesEditor = createEditor("variablesEditor", "json");
+  var responseEditor = createEditor("responseEditor", "json", true);
 
-  queryEditor.setValue(initialQuery, -1)
+  queryEditor.setValue(initialQuery, -1);
 
   var updateFromUrl = function (url) {
-    var params = new URI(url ? url : document.location).search(true)
+    var params = new URI(url ? url : document.location).search(true);
 
     if (params.query)
-      queryEditor.setValue(params.query, -1)
+      queryEditor.setValue(params.query, -1);
 
     if (params.args) {
-      variablesEditor.setValue(params.args, -1)
+      variablesEditor.setValue(params.args, -1);
       $("#variablesSection").collapse('show')
     } else {
-      variablesEditor.setValue("", -1)
+      variablesEditor.setValue("", -1);
       $("#variablesSection").collapse('hide')
     }
 
-    if (params.operation)
-      $("#operation").val(params.operation)
-  }
+    if (params.operationName)
+      $("#operation").val(params.operationName)
+  };
 
   var execute = function () {
     var queryParams = {
       query: queryEditor.getValue()
-    }
+    };
 
     if (variablesEditor.getValue().trim() !== '')
-      queryParams.variables = variablesEditor.getValue().trim()
+      queryParams.variables = variablesEditor.getValue().trim();
 
     if ($("#operation").val() && $("#operation").val().trim !== '')
-      queryParams.operation = $("#operation").val()
+      queryParams.operationName = $("#operation").val();
 
-    var url = $("#graphqlUrl").val()
+    var url = $("#graphqlUrl").val();
 
 
     $.get(url, queryParams)
       .done(function (res) {
-        responseEditor.setValue(JSON.stringify(res, null, 2), -1)
+        responseEditor.setValue(JSON.stringify(res, null, 2), -1);
 
         if (res.errors && res.errors.length > 0) {
-          $("#responseError").html("")
+          $("#responseError").html("");
 
           for (var i = 0; i < res.errors.length; i++) {
             $("#responseError").append($("<pre>").html(res.errors[i].message))
@@ -119,36 +119,36 @@ $(function () {
       })
       .fail(function (error) {
         if (error.status === 400 && error.responseJSON && error.responseJSON.syntaxError)
-          $("#responseError").html($("<pre>").html(error.responseJSON.syntaxError))
+          $("#responseError").html($("<pre>").html(error.responseJSON.syntaxError));
 
-        $("#response").collapse('hide')
+        $("#response").collapse('hide');
         $("#errors").collapse('show')
       })
-  }
+  };
 
   var showShortcuts = function () {
     $("#hotkeysDialog").modal()
-  }
+  };
 
   var toggleVariables = function () {
     $("#variablesSection").collapse('toggle')
-  }
+  };
 
   var toggleConfig = function () {
     $("#configSection").collapse('toggle')
-  }
+  };
 
-  $("#runButton").on("click", execute)
-  $(document).bind("keydown", "shift+/", showShortcuts)
-  $(document).bind("keydown", "ctrl+return", execute)
-  $(document).bind("keydown", "v", toggleVariables)
-  $(document).bind("keydown", "c", toggleConfig)
-  $(".ace_editor textarea").bind("keydown", "ctrl+return", execute)
+  $("#runButton").on("click", execute);
+  $(document).bind("keydown", "shift+/", showShortcuts);
+  $(document).bind("keydown", "ctrl+return", execute);
+  $(document).bind("keydown", "v", toggleVariables);
+  $(document).bind("keydown", "c", toggleConfig);
+  $(".ace_editor textarea").bind("keydown", "ctrl+return", execute);
 
 
-  updateFromUrl()
+  updateFromUrl();
 
-  console.info(History, History.Adapter)
+  console.info(History, History.Adapter);
 
   History.Adapter.bind(window, 'statechange', function () {
     updateFromUrl(History.getState().url)
@@ -156,26 +156,26 @@ $(function () {
 
   var setState = function (query) {
     History.pushState({state: 1}, document.title, new URI().search(query).search())
-  }
+  };
 
   $(".exampleLink").click(function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
     if (examples[e.target.id])
       setState(examples[e.target.id])
-  })
+  });
 
   $("#permalink").click(function () {
     var queryParams = {
       query: queryEditor.getValue()
-    }
+    };
 
     if (variablesEditor.getValue().trim() !== '')
-      queryParams.args = variablesEditor.getValue().trim()
+      queryParams.args = variablesEditor.getValue().trim();
 
     if ($("#operation").val() && $("#operation").val().trim !== '')
-      queryParams.operation = $("#operation").val()
+      queryParams.operationName = $("#operation").val();
 
     History.pushState({state: 1}, document.title, new URI().search(queryParams).search())
   })
-})
+});
